51f5571fbf821f6886ae01355e117d399bc3c1dd
WeirdReaper2006 <rayyanrafique95@gmail.com>
2024-07-02 23:04:19 +0500

    Added feature to have ids of each student

--- Grade_management_System.py
+++ Grade_management_System.py
@@ -1,15 +1,27 @@
 StudentInfo = []
+existing_student_ids = set()
+filehandle = open("IDs.txt", "r")
+for i in filehandle.readlines():
+    existing_student_ids.add(int(i.strip()))
 
 class Student:
-    def __init__(self, n, m):
+    def __init__(self, id, n, m):
+        self.__ID = int(id)
         self.__name = str(n)
         self.__marks = int(m)
+        
+    def getStudentID(self):
+        return self.__ID
         
     def getStudentName(self):
         return self.__name
     
     def getStudentMarks(self):
         return self.__marks
+    
+    def getStudentDetails(self):
+        retVal = f"{self.__name} with student ID {self.__ID} got {self.__marks} marks"
+        return retVal
     
 def ValidateName(name):
     retVal = True
@@ -28,16 +40,27 @@ def CalcAvg():
     print (total/count)
     
 def AddStudent():
+    global existing_student_ids
+    id = int(input("Enter Student ID: "))
+    while id < 1 or id in existing_student_ids:
+        if id < 1:
+            id = int(input("You have entered an invalid ID. Enter Student ID: "))
+        elif id in existing_student_ids:
+            id = int(input("This ID is already taken. Enter a different Student ID: "))
     name = str(input("Enter name of student: "))
     while ValidateName(name) == False:
         name = str(input("You have entered a wrong value. Enter name of student again: "))
     marks = int(input("Enter marks of student: "))
     while marks < 0 or marks > 100:
         marks = int(input("You entered a wrong value. Enter marks of student: "))
-    student = Student(name, marks)
+    student = Student(id, name, marks)
+    existing_student_ids.add(id)
     fileHandle = open("Grades.txt", "a")
-    fileHandle.write(f"{name} got {marks}\n")
+    fileHandle.write(f"{name} with student ID {id} got {marks}\n")
     fileHandle.close()
+    fileHandle2 = open("IDs.txt", "a")
+    fileHandle2.write(f"{id}\n")
+    fileHandle2.close()
     StudentInfo.append(student)
     
 #Main Program
@@ -45,4 +68,4 @@ AddStudent()
 AddStudent()
 CalcAvg()
 for i in range(len(StudentInfo)):
-    print(StudentInfo[i], StudentInfo[i].getStudentName(), StudentInfo[i].getStudentMarks())
+    print(StudentInfo[i].getStudentDetails())
